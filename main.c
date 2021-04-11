03ebf6b09f45e14fe775d034c2fe67efcc3eaf69
mrpetrix <xxi@gmx.net>
2021-04-11 12:21:29 +0200

    initial commit w/ header and test program

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "my_utils.h"
+
+int    main(void)
+{
+    char c;
+    char newLine;
+
+    c = 'a';
+    newLine = 10;
+    my_putchar(c);
+    my_putchar(newLine);
+    return(0);
+}
---
+++ my_utils.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+void    my_putchar(char c);
+
+#endif /* MYHEADER_H */
