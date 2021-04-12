2b8e2c26c45827b4b718020e4dff65508e2c7076
mrpetrix <xxi@gmx.net>
2021-04-12 18:25:22 +0200

    add atoi

---
+++ my_atoi.c
@@ -0,0 +1,29 @@
+#include "my_utils.h"
+
+int     my_atoi(char *str)
+{
+    unsigned int    result;
+    int             factor;
+
+    result = 0;
+    factor = 1;
+
+    if (*str == '-')
+    {
+        factor = -1;
+        str++;
+    }
+    else if (*str == '+')
+    {
+        str++;
+    }
+
+    while (*str)
+    {
+        result *= 10;
+        result += *str - 48;
+        str++;
+    }
+
+    return (result * factor);
+}
--- my_utils.h
+++ my_utils.h
@@ -4,5 +4,6 @@
 void    my_putchar(char c);
 void    my_putstr(char *str);
 void    my_putnbr(int nbr);
+int     my_atoi(char *str);
 
 #endif /* MYHEADER_H */
